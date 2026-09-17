8b650fe593e0982505c23a857a85e8f1f7df8311
32-Bit <103079138+ThirtyTwoBit@users.noreply.github.com>
2026-09-17 13:13:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='imgbyte',
-    version='0.1.1',
+    version='0.1.2',
     author='32-Bit',
     description='Automation library for interacting with Imgflip using Selenium and Requests',
     long_description=open('README.md').read(),
